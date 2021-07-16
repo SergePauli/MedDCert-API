181e2f46ada051715a174fe00ac3bdfba7bec715
SergePauli <SergePauli@yandex.ru>
2021-07-16 11:04:07 +0800

    fix: (config/route) change auth routes for support api type and version-info(REST_API/v1/auth)  nonstable

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   # Авторизация
-  scope "auth", controller: "users" do
+  scope "REST_API/v1/auth", controller: "rest_api/v1/auth/users" do
     post "/registration", action: "registration"
     post "/login", action: "login"
     post "/logout", action: "logout"
