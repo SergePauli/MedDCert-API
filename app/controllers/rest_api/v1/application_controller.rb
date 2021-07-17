5b97ee7512a664bdb70b9ef5d547a0d0a3f938dc
SergePauli <SergePauli@yandex.ru>
2021-07-17 12:43:07 +0800

    feature:(add [authenticate_request, current_user] to application_ controller), nonstable

--- app/controllers/rest_api/v1/application_controller.rb
+++ app/controllers/rest_api/v1/application_controller.rb
@@ -1,4 +1,7 @@
 class RestApi::V1::ApplicationController < ActionController::API
+  before_action :authenticate_request
+  attr_reader :current_user
+
   ## API Universal Exception Handling
   class ApiError < StandardError
     def initialize(message, status)
@@ -13,5 +16,13 @@ class RestApi::V1::ApplicationController < ActionController::API
 
   def api_error(exception)
     render json: { errors: [exception.message] }, status: exception.status
+  end
+
+  def authorize_request
+    header = request.headers["Authorization"]
+    raise ApiError.new("Не авторизовано", :unauthorized) unless header
+    header = header.split(" ").last
+    @current_user = TokensService.validate_token(header, SECRET[:access_token])
+    raise ApiError.new("Не авторизовано", :unauthorized) unless @current_user
   end
 end
