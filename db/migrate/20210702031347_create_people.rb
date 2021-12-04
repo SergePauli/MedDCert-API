d61483d39688445ab39d61c43b7cd438d5686009
SergePauli <SergePauli@yandex.ru>
2021-12-04 12:11:33 +0800

    fix:(change CreatePeople migration ), stable

--- db/migrate/20210702031347_create_people.rb
+++ db/migrate/20210702031347_create_people.rb
@@ -1,9 +1,13 @@
-class CreatePerson < ActiveRecord::Migration[6.1]
-  def change
+class CreatePeople < ActiveRecord::Migration[6.1]
+  def up
     create_table :people, primary_key: "id", id: :uuid, default: -> { "gen_random_uuid()" } do |t|
       t.belongs_to :person_name, index: true, null: false, comment: "ФИО"
       t.string :SNILS, index: true, comment: "номер СНИЛС"
       t.timestamps
     end
+  end
+
+  def down
+    drop_table :people
   end
 end
