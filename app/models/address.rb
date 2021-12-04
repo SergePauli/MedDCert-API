8854a47b9dd1a92129662d6b20241890d12c4508
SergePauli <SergePauli@yandex.ru>
2021-12-04 11:34:13 +0800

    feature:(add associations and permitted_params to Address model), stable

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,2 +1,12 @@
 class Address < ApplicationRecord
+  has_many :null_flavors, class_name: "NullFlavor", primary_key: "id", foreign_key: "parent_guid"
+  accepts_nested_attributes_for :null_flavors, reject_if: ->(attributes) { attributes["parent_attr"].blank? || attributes["code"].blank? }, allow_destroy: true
+  # For using in UniversalEnttityController or other models
+  # Для использования в универсальном контроллере сущностей
+  # и в других моделях
+  def self.permitted_params
+    [:id, :state, :streetAddressLine, :aoGUID, :houseGUID, :postalCode,
+     :code, :parent_guid, :actual, :house_number, :struct_number, :building_number,
+     :flat_number, null_flavor_attributes: NullFlavor.permitted_params]
+  end
 end
