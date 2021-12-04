2254dbab7618facafc9131cd52c8fc691f2aed10
SergePauli <SergePauli@yandex.ru>
2021-12-04 12:41:26 +0800

    feature:(improve CreateDiagnoses migration to Up and Down), stable

--- db/migrate/20210706142243_create_diagnoses.rb
+++ db/migrate/20210706142243_create_diagnoses.rb
@@ -1,9 +1,13 @@
 class CreateDiagnoses < ActiveRecord::Migration[6.1]
-  def change
+  def up
     create_table :diagnoses, comment: "справочник МКБ-10" do |t|
       t.string :s_name, index: true, comment: "наименование"
       t.integer :sort, comment: "сортировка"
       t.string :ICD10, index: true, comment: "код ICD10"
     end
+  end
+
+  def down
+    drop_table :diagnoses
   end
 end
