1f15e3c685b8a0f4c7492ce88dd7b46e8aeff7f0
SergePauli <SergePauli@yandex.ru>
2021-12-04 12:38:12 +0800

    feature:(add self.permitted_params to NullFlavor model), stable

--- app/models/null_flavor.rb
+++ app/models/null_flavor.rb
@@ -5,4 +5,10 @@ class NullFlavor < ApplicationRecord
                UNC: "Кодирование не проводилось", MSK: "Скрыто", NA: "Неприменимо", UNK: "Неизвестно",
                ASKU: "Запрошено, но неизвестно", NAV: "Временно недоступно", NASK: "Не запрашивалось",
                QS: "Достаточное количество", TRC: "Трудноразличимо" }.freeze
+  # For using in UniversalEnttityController or other models
+  # Для использования в универсальном контроллере сущностей
+  # и в других моделях
+  def self.permitted_params
+    [:id, :parent_guid, :parent_attr, :code]
+  end
 end
