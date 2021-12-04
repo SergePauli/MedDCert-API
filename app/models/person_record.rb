7432a88bff40a583411a3c7c1303f2ded2280927
SergePauli <SergePauli@yandex.ru>
2021-12-04 12:25:21 +0800

    feature:(create PersonRecord abstract class for person_names models), stable

---
+++ app/models/person_record.rb
@@ -0,0 +1,24 @@
+# for using in models belongs to person_name
+class PersonRecord < ActiveRecord::Base
+  self.abstract_class = true
+
+  belongs_to :person_name
+
+  accepts_nested_attributes_for :person_name, reject_if: ->(attributes) { attributes["family"].blank? || attributes["given_1"].blank? }
+
+  def name
+    "#{person_name.family} #{person_name.given_1} #{person_name.given_2}"
+  end
+
+  after_initialize do |person|
+    if person.person_name.id == nil && person.person_name
+      exist_person_name = PersonName.where(family: person.person_name.family).where(given_1: person.person_name.given_1).where(given_2: person.person_name.given_2)
+      person.person_name = exist_person_name.first if exist_person_name && exist_person_name.first
+      puts "after_initialize", person.person_name.id
+    end
+  end
+
+  def self.permitted_params
+    [person_name_attributes: [:id, :family, :given_1, :given_2]]
+  end
+end
