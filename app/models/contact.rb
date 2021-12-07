bf2ea3a749111b580d22bcce856906979311055b
SergePauli <SergePauli@yandex.ru>
2021-12-07 13:11:17 +0800

    feature:(add :_destroy field to permitted_params )

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,5 +1,5 @@
 class Contact < ApplicationRecord
   def self.permitted_params
-    [:id, :parent_guid, :telcom_value, :telcom_use, :main]
+    [:id, :parent_guid, :telcom_value, :telcom_use, :main, :_destroy]
   end
 end
