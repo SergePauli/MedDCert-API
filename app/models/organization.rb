be6cb79b5330de7574b9c4166df5176ab516f407
SergePauli <SergePauli@yandex.ru>
2021-12-15 22:41:06 +0800

    feature:(make Organization model tracable), stable

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,10 +1,19 @@
 class Organization < ApplicationRecord
+  #  audit needed
+  def self.trackable?
+    true
+  end
+
   has_one :address, primary_key: "guid", foreign_key: "parent_guid", autosave: true, dependent: :destroy
   accepts_nested_attributes_for :address
   has_many :contacts, primary_key: "guid", foreign_key: "parent_guid", autosave: true, dependent: :destroy
   accepts_nested_attributes_for :contacts
 
   def to_s
-    "MO[#{id}] #{organization.name || organization.name_full}"
+    "MO[#{id}] #{name || name_full}"
+  end
+
+  def self.permitted_params
+    [:id, :guid, :name, :name_full, :license, :license_data, :okpo, contacts_attributes: Contact.permitted_params, address_attributes: Address.permitted_params]
   end
 end
