e0f4b2ea35659d9e8bebaff52a1bd68ec8ab9070
SergePauli <SergePauli@yandex.ru>
2022-01-10 13:47:36 +0800

    fix:(make reversible ExternalReasons migration and remove certificate_id field from it)need rollup and aply it again

--- db/migrate/20210709013536_create_external_reasons.rb
+++ db/migrate/20210709013536_create_external_reasons.rb
@@ -1,10 +1,13 @@
 class CreateExternalReasons < ActiveRecord::Migration[6.1]
-  def change
-    create_table :external_reasons, primary_key: "id", id: :string, comment: "Внешние причины смерти" do |t|
-      t.belongs_to :certificate, null: false, comment: "ссылка на свидетельство"
+  def up
+    create_table :external_reasons, comment: "Внешние причины смерти" do |t|
       t.belongs_to :ext_diagnosis, null: false, comment: "код мкб-10"
       t.datetime :effective_time, comment: "период времени"
       t.timestamps
     end
+  end
+
+  def down
+    drop_table :external_reasons
   end
 end
