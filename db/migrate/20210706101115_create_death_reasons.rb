e42499bea66311b6634c4d8a7261a6104677dc7a
SergePauli <SergePauli@yandex.ru>
2022-01-10 13:49:33 +0800

    fix:(using standart ID in DeathReasons migration) need rollup and run it again

--- db/migrate/20210706101115_create_death_reasons.rb
+++ db/migrate/20210706101115_create_death_reasons.rb
@@ -1,10 +1,14 @@
 class CreateDeathReasons < ActiveRecord::Migration[6.1]
-  def change
-    create_table :death_reasons, primary_key: "id", id: :string, comment: "Причины смерти" do |t|
+  def up
+    create_table :death_reasons, comment: "Причины смерти" do |t|
       t.belongs_to :certificate, null: false, comment: "ссылка на свидетельство"
       t.belongs_to :diagnosis, null: false, comment: "код мкб-10"
       t.datetime :effective_time, comment: "период времени"
       t.timestamps
     end
+  end
+
+  def down
+    drop_table :death_reasons
   end
 end
