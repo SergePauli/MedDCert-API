5f80b61541078e7d0ede49732f568435c1be4cc8
SergePauli <SergePauli@yandex.ru>
2022-01-13 10:21:14 +0800

    feature:(add permitted params to Authenticator model), stable

--- app/models/authenticator.rb
+++ app/models/authenticator.rb
@@ -1,2 +1,10 @@
 class Authenticator < ApplicationRecord
+  # Связь с записью данных медработника
+  belongs_to :doctor
+
+  # For using in UniversalEnttityController or other models
+  # Для использования в универсальном контроллере сущностей
+  def self.permitted_params
+    [:id, :time, :doctor_id, :_destroy]
+  end
 end
