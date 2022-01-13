7c23276c0b79fd35a3daf65e644b7eeb6b8f995c
SergePauli <SergePauli@yandex.ru>
2022-01-13 10:22:52 +0800

    feature:(add functionality to ChildInfo model), stable

--- app/models/child_info.rb
+++ app/models/child_info.rb
@@ -1,2 +1,23 @@
 class ChildInfo < ApplicationRecord
+  # ----дочерние записи--------
+
+  # Заполнители причин отсутствия
+  has_many :null_flavors, class_name: "NullFlavor", primary_key: "guid", foreign_key: "parent_guid", dependent: :destroy
+  accepts_nested_attributes_for :null_flavors, allow_destroy: true
+  # инфо по матери ребенка до месяца
+  has_one :related_subject, class_name: "RelatedSubject", primary_key: "certificate_id", foreign_key: "certificate_id", dependent: :destroy
+  accepts_nested_attributes_for :related_subject, allow_destroy: true
+
+  def to_s
+    "Ребенок до года [#{:certificate_id}] вес: #{weight}(г.) #{which_account}- по счету"
+  end
+
+  # For using in UniversalEnttityController or other models
+  # Для использования в универсальном контроллере сущностей
+  # и в других моделях
+  def self.permitted_params
+    [:term_pregnancy, :weight, :which_account, :_destroy,
+     related_subject_attributes: RelatedSubject.permitted_params,
+     null_flavors_attributes: NullFlavor.permitted_params]
+  end
 end
