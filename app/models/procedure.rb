aed796ec3b649ae69b63b3048521b458abbe3bf1
SergePauli <SergePauli@yandex.ru>
2022-01-13 10:36:08 +0800

    feature:(add functionality to Procedures model), stable

--- app/models/procedure.rb
+++ app/models/procedure.rb
@@ -1,2 +1,17 @@
 class Procedure < ApplicationRecord
+  #-------------------------Связи-----------------------------------------
+
+  # Связь с медуслугой
+  belongs_to :medical_serv
+
+  # Заполнители причин отсутствия
+  has_many :null_flavors, class_name: "NullFlavor", primary_key: "guid", foreign_key: "parent_guid", dependent: :destroy
+  accepts_nested_attributes_for :null_flavors, allow_destroy: true
+
+  # For using in UniversalEnttityController or other models
+  # Для использования в универсальном контроллере сущностей
+  def self.permitted_params
+    [:id, :guid, :medical_serv_id, :effective_time, :_destroy,
+     null_flavors_attributes: NullFlavor.permitted_params]
+  end
 end
