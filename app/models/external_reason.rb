7c53ad134377e1abbe86bf206376f5e973378a15
SergePauli <SergePauli@yandex.ru>
2022-01-13 10:25:00 +0800

    feature:(add functionality to ExternalReason model), stable

--- app/models/external_reason.rb
+++ app/models/external_reason.rb
@@ -1,2 +1,11 @@
 class ExternalReason < ApplicationRecord
+  # Связь с диагнозом МКБ10
+  belongs_to :ext_diagnosis
+
+  # For using in UniversalEnttityController or other models
+  # Для использования в универсальном контроллере сущностей
+  def self.permitted_params
+    [:id, :ext_diagnosis_id, :effective_time, :_destroy,
+     null_flavors_attributes: NullFlavor.permitted_params]
+  end
 end
