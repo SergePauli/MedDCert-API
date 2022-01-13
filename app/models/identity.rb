9fc1dc6c9c76457d1c49170f68714f1b708a3186
SergePauli <SergePauli@yandex.ru>
2022-01-13 10:30:06 +0800

    feature:(add functionality to Identity model), stable

--- app/models/identity.rb
+++ app/models/identity.rb
@@ -1,2 +1,16 @@
 class Identity < ApplicationRecord
+  # Связь с записью типа документа УЛ
+  belongs_to :identity_card_type
+  # Заполнители причин отсутствия
+  has_many :null_flavors, class_name: "NullFlavor", primary_key: "id", foreign_key: "parent_guid", dependent: :destroy
+  accepts_nested_attributes_for :null_flavors, allow_destroy: true
+
+  # For using in UniversalEnttityController or other models
+  # Для использования в универсальном контроллере сущностей
+  # и в других моделях
+  def self.permitted_params
+    [:id, :identity_card_type_id, :series, :number, :issueOrgName, :issueOrgCode,
+     :issueDate, :_destroy,
+     null_flavors_attributes: NullFlavor.permitted_params]
+  end
 end
