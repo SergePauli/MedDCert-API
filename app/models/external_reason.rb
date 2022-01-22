e2e79c7dc7bf89650a1a262bebe65d51351dfb04
SergePauli <SergePauli@yandex.ru>
2022-01-22 15:55:07 +0800

    fix:(add guid field to external_reason migration, add nullFlavors support to ExternalReason model),

--- app/models/external_reason.rb
+++ app/models/external_reason.rb
@@ -1,6 +1,9 @@
 class ExternalReason < ApplicationRecord
   # Связь с диагнозом МКБ10
   belongs_to :ext_diagnosis
+  # Заполнители причин отсутствия
+  has_many :null_flavors, class_name: "NullFlavor", primary_key: "guid", foreign_key: "parent_guid", dependent: :destroy
+  accepts_nested_attributes_for :null_flavors, allow_destroy: true
 
   # For using in UniversalEnttityController or other models
   # Для использования в универсальном контроллере сущностей
--- db/migrate/20210709013536_create_external_reasons.rb
+++ db/migrate/20210709013536_create_external_reasons.rb
@@ -4,6 +4,7 @@ class CreateExternalReasons < ActiveRecord::Migration[6.1]
       t.belongs_to :certificate, class_name: "Certificate"
       t.belongs_to :ext_diagnosis, null: false, comment: "код мкб-10"
       t.datetime :effective_time, comment: "период времени"
+      t.uuid :guid, unique: true, null: false, default: -> { "gen_random_uuid()" }
       t.timestamps
     end
   end
