53c8cd2b00ff81443500841a25f50df37d6884a8
SergePauli <SergePauli@yandex.ru>
2022-01-27 09:17:30 +0800

    fix:(add terms attributes like years, days, etc), stable

--- db/migrate/20210706101115_create_death_reasons.rb
+++ db/migrate/20210706101115_create_death_reasons.rb
@@ -4,7 +4,13 @@ class CreateDeathReasons < ActiveRecord::Migration[6.1]
       t.string :type, index: true, comment: "тип причины, обеспечение STI"
       t.belongs_to :certificate, index: true, comment: "ссылка на свидетельство"
       t.belongs_to :diagnosis, null: false, comment: "код мкб-10"
-      t.datetime :effective_time, comment: "период времени"
+      t.datetime :effective_time, comment: "дата возникновения состояния"
+      t.integer :years, limit: 1, comment: "длительность состояния, лет"
+      t.integer :months, limit: 1, comment: "длительность состояния, месяцев"
+      t.integer :weeks, limit: 1, comment: "длительность состояния, недель"
+      t.integer :days, limit: 1, comment: "длительность состояния, дней"
+      t.integer :hours, limit: 1, comment: "длительность состояния, часов"
+      t.integer :minutes, limit: 1, comment: "длительность состояния, минут"
       t.uuid :guid, unique: true, null: false, default: -> { "gen_random_uuid()" }
       t.timestamps
     end
--- db/migrate/20210709013536_create_external_reasons.rb
+++ db/migrate/20210709013536_create_external_reasons.rb
@@ -4,6 +4,12 @@ class CreateExternalReasons < ActiveRecord::Migration[6.1]
       t.belongs_to :certificate, class_name: "Certificate"
       t.belongs_to :ext_diagnosis, null: false, comment: "код мкб-10"
       t.datetime :effective_time, comment: "период времени"
+      t.integer :years, limit: 1, comment: "длительность состояния, лет"
+      t.integer :months, limit: 1, comment: "длительность состояния, месяцев"
+      t.integer :weeks, limit: 1, comment: "длительность состояния, недель"
+      t.integer :days, limit: 1, comment: "длительность состояния, дней"
+      t.integer :hours, limit: 1, comment: "длительность состояния, часов"
+      t.integer :minutes, limit: 1, comment: "длительность состояния, минут"
       t.uuid :guid, unique: true, null: false, default: -> { "gen_random_uuid()" }
       t.timestamps
     end
