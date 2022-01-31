9c8d0fd139380205e9664a8520da09b62dad5fdc
SergePauli <SergePauli@yandex.ru>
2022-01-31 15:25:42 +0800

    fix:(using person optionaly in Patient model), stable

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -6,7 +6,7 @@ class Patient < ApplicationRecord
   has_one :identity, primary_key: "guid", class_name: "Identity", foreign_key: "parent_guid", autosave: true, dependent: :destroy
   accepts_nested_attributes_for :identity, allow_destroy: true
   # Связь с записью персональных данных
-  belongs_to :person, primary_key: "id", foreign_key: "person_id"
+  belongs_to :person, optional: true, autosave: true
   accepts_nested_attributes_for :person
 
   # Заполнители причин отсутствия
@@ -17,7 +17,7 @@ class Patient < ApplicationRecord
   # Для использования в универсальном контроллере сущностей
   # и в других моделях
   def self.permitted_params
-    [:id, :guid, :organization_id, :addr_type, :gender, :birth_date, :birth_year,
+    [:id, :guid, :organization_id, :person_id, :addr_type, :gender, :birth_date, :birth_year,
      identity_attributes: Identity.permitted_params,
      person_attributes: Person.permitted_params,
      null_flavors_attributes: NullFlavor.permitted_params]
