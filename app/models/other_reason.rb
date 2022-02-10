1786ac7f59c426df4d1b85e3eca3e169ced6ace7
SergePauli <SergePauli@yandex.ru>
2022-02-10 11:51:42 +0800

    feature:(add printing functionality to OtherReason model), stable

--- app/models/other_reason.rb
+++ app/models/other_reason.rb
@@ -3,6 +3,23 @@ class OtherReason < DeathReason
   has_many :procedures, class_name: "Procedure", primary_key: "id", foreign_key: "death_reason_id", dependent: :destroy
   accepts_nested_attributes_for :procedures, allow_destroy: true
 
+  # Унифицированый вывод расшифровки кода МКБ10 для всех причин
+  def diagnosis_name
+    result = diagnosis.s_name
+    result += procNames if procedures.size > 0
+  end
+
+  # генерируем строку хирургических операций(процедур)
+  def procNames
+    result = " "
+    procedures.each do |proc|
+      result += proc.text_value || proc.medical_serv.name
+      result += " от " + proc.timeStr if proc.effective_time
+      result += "; "
+    end
+    return result
+  end
+
   # For using in UniversalEnttityController or other models
   # Для использования в универсальном контроллере сущностей
   def self.permitted_params
