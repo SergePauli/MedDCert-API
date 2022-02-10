9dcbe33bf7455c3e7b841c01e9e59f063f145e16
SergePauli <SergePauli@yandex.ru>
2022-02-10 10:57:05 +0800

    feature: (add print 106/У functionality), unstable

---
+++ app/controllers/rest_api/v1/print_controller.rb
@@ -0,0 +1,145 @@
+# This is rest-client for FIAS API
+require "rest-client"
+
+class RestApi::V1::PrintController < ActionController::Base
+  #before_action :authenticate_request
+  #before_action :find_record
+
+  def index
+    render template: "print/index"
+  end
+
+  # GET /REST_API/v1/print/face/:id
+  def face
+    find_record
+    @month = age_month
+    if @certificate.patient.person && @certificate.patient.person.address
+      adr = @certificate.patient.person.address
+      @address = copy_address(adr)
+      result = request_address_info(adr.aoGUID, adr.houseGUID ? "building" : "")
+      if result && result["data"].length > 0
+        parse_address_info(result["data"][0])
+        @regAddress = @address.dup
+      else
+        @regAddress = copy_address
+        @regAddress[:nullFlavor] = "ОШИБКА ПЕЧАТИ"
+      end
+    elsif @certificate.patient.person
+      @regAddress = copy_address
+      @regAddress[:nullFlavor] = "НЕИЗВЕСТНО"
+    else
+      @regAddress = copy_address
+      @regAddress[:nullFlavor] = "НЕПРИМЕНИМО"
+    end
+    adr = @certificate.death_addr
+    if adr
+      @address = copy_address(adr)
+      result = request_address_info(adr.aoGUID, adr.houseGUID ? "building" : "")
+      if result && result["data"].length > 0
+        parse_address_info(result["data"][0])
+        @deathAddress = @address.dup
+      else
+        @deathAddress = copy_address
+        @deathAddress[:nullFlavor] = "ОШИБКА ПЕЧАТИ"
+      end
+    else
+      @deathAddress = copy_address
+      @deathAddress[:nullFlavor] = "НЕИЗВЕСТНО"
+    end
+    if !@month.blank? && @certificate.child_info && @certificate.child_info.address
+      adr = @certificate.child_info.address
+      @address = copy_address(adr)
+      result = request_address_info(adr.aoGUID, adr.houseGUID ? "building" : "")
+      if result && result["data"].length > 0
+        parse_address_info(result["data"][0])
+        @childAddress = @address.dup
+      else
+        @childAddress = copy_address
+        @childAddress[:nullFlavor] = "ОШИБКА ПЕЧАТИ"
+      end
+    else
+      @childAddress = copy_address
+      @childAddress[:nullFlavor] = "НЕИЗВЕСТНО"
+    end
+    mother_info
+    render "print/face", layout: "print"
+  end
+
+  # GET /REST_API/v1/print/back/:id
+  def back
+    find_record
+    render "print/back", layout: "print"
+  end
+
+  private
+
+  def find_record
+    @certificate = Certificate.find params[:id]
+  end
+
+  def age_days
+    return "" if @certificate.patient.birth_date.blank? || @certificate.death_datetime.blank?
+    @d_day = Date.parse(@certificate.death_datetime.to_s)
+    @b_day = @certificate.patient.birth_date
+    days = (@d_day - @b_day).to_i
+    return "" if days > 366 # only for child < 1 year
+    return days
+  end
+
+  def age_month
+    @days = age_days
+    return "" if @days.blank?
+    return "" if @days < 30
+    if (@d_day.year > @b_day.year)
+      return @d_day.month + (12 - @b_day.month)
+    else
+      return @d_day.month - @b_day.month
+    end
+  end
+
+  def mother_info
+    @mother_birthday = @certificate.child_info &&
+                       @certificate.child_info.related_subject &&
+                       @certificate.child_info.related_subject.birthTime
+    @mother_name = @certificate.child_info &&
+                   @certificate.child_info.related_subject &&
+                   @certificate.child_info.related_subject.name
+    if @days > 30 || @mother_birthday.blank?
+      @mother_age = ""
+    else
+      @mother_age = Date.today.year - @mother_birthday.year
+    end
+  end
+
+  def request_address_info(parent, level)
+    # заголовки в запросе
+    headers = { content_type: :json, accept: :json }
+    # Параметры подключения к ФИАС сервису
+    url_string = "http://localhost:5050/fias?parent=#{parent}&withParent=1&level=#{level}&limit=1"
+    r = RestClient::Request.execute(method: :get, url: url_string, headers: headers)
+    return JSON.parse(r)
+  end
+
+  def copy_address(address = nil)
+    {
+      nullFlavor: false, region: "", district: "", city: "", town: "",
+      street: "", house_number: address && address.house_number || "", struct_number: address && address.struct_number || "", building_number: address && address.building_number || "", flat_number: address && address.flat_number || "",
+    }
+  end
+
+  def parse_address_info(fias_item)
+    case fias_item["level"]
+    when "Street"
+      @address[:street] = "#{fias_item["name"]} #{(fias_item["shortname"].include? "ул") ? "" : fias_item["shortname"]}"
+    when "Town"
+      @address[:town] = "#{fias_item["shortname"]} #{fias_item["name"]}"
+    when "City"
+      @address[:city] = fias_item["name"]
+    when "District"
+      @address[:distric] = fias_item["name"]
+    when "Region"
+      @address[:region] = "#{fias_item["name"]} #{fias_item["shortname"]}"
+    end
+    parse_address_info(fias_item["parent"]) if fias_item["parent"]
+  end
+end
