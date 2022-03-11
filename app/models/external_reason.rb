6d2e25998bc73c7716a89a07fa2a8e6f15f50499
SergePauli <SergePauli@yandex.ru>
2022-03-11 18:59:31 +0800

    temporary:(add DeathReathon common functionality), stable

--- app/models/external_reason.rb
+++ app/models/external_reason.rb
@@ -1,4 +1,4 @@
-class ExternalReason < ApplicationRecord
+class ExternalReason < NullFlavorRecord
   # Связь с диагнозом МКБ10
   belongs_to :ext_diagnosis
   # Заполнители причин отсутствия
@@ -7,6 +7,42 @@ class ExternalReason < ApplicationRecord
 
   def diagnosis
     ext_diagnosis
+  end
+
+  # Период времени
+  def terms_to_s
+    result = ""
+    result += "#{years} года " if !years.blank? && years < 5
+    result += "#{years} лет " if !years.blank? && years > 4
+    result += "#{months} мес. " if !months.blank?
+    result += "#{weeks} нед. " if !weeks.blank?
+    result += "#{days} дн. " if !days.blank?
+    result += "#{hours} ч. " if !hours.blank?
+    result += "#{minutes} мин. " if !minutes.blank?
+    return result
+  end
+
+  # проверяет, внесено ли время между началом состояния и смертью
+  def is_effective_time?
+    !(years.blank? && months.blank? && days.blank? && hours.blank? && minutes.blank?)
+  end
+
+  # возвращает effective_time в нужном для CDA формате:
+  # "YYYYMMDD[HHMM[SS]+|-ZZZZ]"
+  def fmt_effective_time(death_time)
+    start_time = death_time
+    start_time = start_time - (years).years if !years.blank?
+    start_time = start_time - (months).months if !months.blank?
+    start_time = start_time - (weeks).weeks if !weeks.blank?
+    start_time = start_time - (days).days if !days.blank?
+    start_time = start_time - (hours).hours if !hours.blank?
+    start_time = start_time - (minutes).minutes if !minutes.blank?
+    if hours || minutes
+      time_zone = DateTime.now.to_s.slice(-6, 6).gsub(":", "")
+      return start_time.strftime("%Y%m%d%k%M") + time_zone
+    else
+      return start_time.strftime("%Y%m%d%k%M")
+    end
   end
 
   # Унифицированый вывод расшифровки кода МКБ10 для всех причин
