44b65998e8b25f60c061e167732a14ee26cf6d64
SergePauli <SergePauli@yandex.ru>
2022-03-13 13:08:54 +0800

    refactor:(organize Death Reasons Models to class Hierarchy), stable

--- app/models/death_reason.rb
+++ app/models/death_reason.rb
@@ -1,5 +1,6 @@
-class DeathReason < ApplicationRecord
-  #-------------------------Связи-------------------------------------------
+# Базовая STI модель для всех внутренних причин смерти и состояний
+class DeathReason < ReasonRecord
+  #-------------------------Связи--------------------------------
 
   # Связь с диагнозом МКБ10
   belongs_to :diagnosis
@@ -7,24 +8,6 @@ class DeathReason < ApplicationRecord
   # Заполнители причин отсутствия
   has_many :null_flavors, class_name: "NullFlavor", primary_key: "guid", foreign_key: "parent_guid", dependent: :destroy
   accepts_nested_attributes_for :null_flavors, allow_destroy: true
-
-  # Унифицированый вывод расшифровки кода МКБ10 для всех причин
-  def diagnosis_name
-    diagnosis.s_name
-  end
-
-  # Период времени
-  def terms_to_s
-    result = ""
-    result += "#{years} года " if !years.blank? && years < 5
-    result += "#{years} лет " if !years.blank? && years > 4
-    result += "#{months} мес. " if !months.blank?
-    result += "#{weeks} нед. " if !weeks.blank?
-    result += "#{days} дн. " if !days.blank?
-    result += "#{hours} ч. " if !hours.blank?
-    result += "#{minutes} мин. " if !minutes.blank?
-    return result
-  end
 
   # For using in UniversalEnttityController or other models
   # Для использования в универсальном контроллере сущностей
--- app/models/external_reason.rb
+++ app/models/external_reason.rb
@@ -1,48 +1,14 @@
-class ExternalReason < NullFlavorRecord
+class ExternalReason < ReasonRecord
   # Связь с диагнозом МКБ10
   belongs_to :ext_diagnosis
+
   # Заполнители причин отсутствия
   has_many :null_flavors, class_name: "NullFlavor", primary_key: "guid", foreign_key: "parent_guid", dependent: :destroy
   accepts_nested_attributes_for :null_flavors, allow_destroy: true
 
+  # Реализация частного геттера для диагноза
   def diagnosis
     ext_diagnosis
-  end
-
-  # Период времени
-  def terms_to_s
-    result = ""
-    result += "#{years} года " if !years.blank? && years < 5
-    result += "#{years} лет " if !years.blank? && years > 4
-    result += "#{months} мес. " if !months.blank?
-    result += "#{weeks} нед. " if !weeks.blank?
-    result += "#{days} дн. " if !days.blank?
-    result += "#{hours} ч. " if !hours.blank?
-    result += "#{minutes} мин. " if !minutes.blank?
-    return result
-  end
-
-  # проверяет, внесено ли время между началом состояния и смертью
-  def is_effective_time?
-    !(years.blank? && months.blank? && days.blank? && hours.blank? && minutes.blank?)
-  end
-
-  # возвращает effective_time в нужном для CDA формате:
-  # "YYYYMMDD[HHMM[SS]+|-ZZZZ]"
-  def fmt_effective_time(death_time)
-    start_time = death_time
-    start_time = start_time - (years).years if !years.blank?
-    start_time = start_time - (months).months if !months.blank?
-    start_time = start_time - (weeks).weeks if !weeks.blank?
-    start_time = start_time - (days).days if !days.blank?
-    start_time = start_time - (hours).hours if !hours.blank?
-    start_time = start_time - (minutes).minutes if !minutes.blank?
-    if hours || minutes
-      time_zone = DateTime.now.to_s.slice(-6, 6).gsub(":", "")
-      return start_time.strftime("%Y%m%d%k%M") + time_zone
-    else
-      return start_time.strftime("%Y%m%d%k%M")
-    end
   end
 
   # Унифицированый вывод расшифровки кода МКБ10 для всех причин
